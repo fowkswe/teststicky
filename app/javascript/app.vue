0be4c0ff85e74c40dd8d4d0b3ae18e6c10b1944a
Billy Fowks <fowkswe@gmail.com>
2018-09-17 10:05:15 -0500

    initial

---
+++ app/javascript/app.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+  #app
+    p(v-for="item in ['before', 'sticky', 'enabled']" :key="item") {{ item }}
+    nav(style="position: sticky; top: 20px; z-index: 101;")
+      ul
+        li
+          a(href='#') This
+        li
+          a(href='#') Nav, created in the hello_vue component 
+        li
+          a(href='#') Wont
+        li
+          a(href='#') Stick
+      p(v-for="item in 100" :key="item") {{ item }}
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+nav {
+  position: sticky;
+  top: 0px;
+}
+</style>
